e14f87feb0125196685b2c0407b86fd00ff5ce6e
moenage <moenage7@yahoo.com>
2022-12-04 20:04:32 -0800

    Added 400 and 404 error checkers

--- singleT_server.py
+++ singleT_server.py
@@ -9,7 +9,7 @@ def createSocket(hostName, portNumber):
 
 
 def main():
-    print('Hello World')
+    # print('Hello World')
 
     # Stores the test.html into a string so we can send to the browser as a respone
     testFile = open('test.html')
@@ -28,13 +28,31 @@ def main():
         (clientSocket, address) = mySocket.accept()
 
         sentence = clientSocket.recv(1024).decode()
-        print(sentence)
-        # clientsocket.send(sentence.encode())
+
+        # 400 Bad Request Checker
+        if not sentence:
+            clientSocket.sendall("HTTP/1.1 400 Bad Request\n" + sentence)
+            break
+
+        sent_partition = sentence.partition('/test.html')
+
+        # 404 File not found error checkers
+        if(sent_partition[1] == "/test.html"):
+            print("1")
+            if(sent_partition[2][0] != " "):
+                print("2")
+                clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + sentence).encode())
+                break
+        elif(sent_partition[1] != "/test.html"):
+            print("3")
+            clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + sentence).encode())
+            break
 
         clientSocket.sendall(("HTTP/1.1 200 OK\n" + testFile_html).encode())
 
         clientSocket.close()
-
+        
+    mySocket.close()
 
 
 main()
