d1d070aa3cc2db33a98df429977bdf790079a993
moenage <moenage7@yahoo.com>
2022-12-05 19:44:23 -0800

    Figured out how to test timeout checker

--- singleT_server.py
+++ singleT_server.py
@@ -24,32 +24,34 @@ def main():
 
     mySocket.listen(1)
 
-    timeLeft = 5 # 5 Seconds set for timeout
+    # timeLeft = 0.000000000000000001 # Time used to test the 408 Timed Out CHecker
+    timeLeft = 5 # 5 seconds used for timeout
 
     while True:
 
         (clientSocket, address) = mySocket.accept()
 
-        whatReady = select.select([clientSocket], [], [], timeLeft)
-        if whatReady[0]:
-            sentence = clientSocket.recv(1024).decode()
-        elif whatReady[0] == []: # 408 Request Timed Out
-            clientSocket.sendall(("HTTP/1.1 408 Request Timed Out\n" + sentence).encode())
+        html_msg = clientSocket.recv(1024).decode()
 
-        GET_partition = sentence.partition('GET')
-        file_partition = sentence.partition('/test.html')
+        GET_partition = html_msg.partition('GET')
+        file_partition = html_msg.partition('/test.html')
+
+        # 408 Request Timed Out Checker
+        whatReady = select.select([clientSocket], [], [], timeLeft)
+        if whatReady[0] == []: 
+            clientSocket.sendall(("HTTP/1.1 408 Request Timed Out\n" + html_msg).encode())
 
         # 400 Bad Request Checker
-        if (GET_partition[1] == "GET") and (GET_partition[2][0] != " "):
-            clientSocket.sendall(("HTTP/1.1 400 Bad Request\n" + sentence).encode())
+        elif (GET_partition[1] == "GET") and (GET_partition[2][0] != " "):
+            clientSocket.sendall(("HTTP/1.1 400 Bad Request\n" + html_msg).encode())
         elif (GET_partition[1] != "GET"):
-            clientSocket.sendall(("HTTP/1.1 400 Bad Request\n" + sentence).encode())
+            clientSocket.sendall(("HTTP/1.1 400 Bad Request\n" + html_msg).encode())
 
         # 404 File not found error checkers
         elif ((file_partition[1] == "/test.html") and (file_partition[2][0] != " ")):
-            clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + sentence).encode())
+            clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + html_msg).encode())
         elif (file_partition[1] != "/test.html"):
-            clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + sentence).encode())
+            clientSocket.sendall(("HTTP/1.1 404 Not Found\n" + html_msg).encode())
 
         # 200 OK
         else:
